d8705197210a7289aa9bf37882384aacffe5b542
Fabrício Barbosa Viegas <49844765+fabriciobarbosaviegas@users.noreply.github.com>
2019-09-28 18:39:46 -0300

    Update code.js

--- Senssor de manha tarde e noite/js/code.js
+++ Senssor de manha tarde e noite/js/code.js
@@ -1,20 +1,22 @@
- 		var atual = new Date()
- 		var turno = atual.getHours()
- 		var t = document.getElementById('t')
- 		var fundo = document.getElementById('fundo')
- 		var img = document.getElementById('imagen')
- 		if(turno < 12){
- 			t.innerHTML = `Manhã`
- 			fundo.style.background = '#dbd655'
- 			img.src = 'manha.jpg'
- 		}
-  		else if(turno >= 12 && turno < 18){
- 			t.innerHTML = `Tarde`
- 			fundo.style.background = '#d69d2c'
- 			img.src = 'tarde.jpg'
- 		}
- 	 	else{
- 			t.innerHTML = `Noite`
- 			fundo.style.background = '#30302e'
- 			img.src = 'noite.jpg'
- 		}
+const turno = new Date().getHours()
+let t = document.getElementById('t')
+let fundo = document.getElementById('fundo')
+let img = document.getElementById('imagen')
+
+if(turno < 12){
+	t.innerHTML = 'Manhã'
+	fundo.style.background = '#dbd655'
+	img.src = 'manha.jpg'
+}
+
+else if(turno >= 12 && turno < 18){
+	t.innerHTML = 'Tarde'
+	fundo.style.background = '#d69d2c'
+	img.src = 'tarde.jpg'
+}
+
+else{
+	t.innerHTML = 'Noite'
+	fundo.style.background = '#30302e'
+	img.src = 'noite.jpg'
+}
